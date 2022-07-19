d7e739fa943ebe568acc5bda01601a47b7671e4e
Pritpal Singh Kandhari <p.p.singh92@gmail.com>
2022-07-19 09:11:00 -0400

    Doctor Signup changes

--- DocConnectAPI/Controllers/SignupController.cs
+++ DocConnectAPI/Controllers/SignupController.cs
@@ -10,6 +10,7 @@ namespace DocConnectAPI.Controllers
 {
     public class SignupController : ApiController
     {
+        [Route("signup", Name = "Signup")]
         [HttpPost]
         public async Task<IHttpActionResult> Signup(SignupModel objSignup)
         {
@@ -21,7 +22,7 @@ namespace DocConnectAPI.Controllers
             SqlDataReader dataReader;
             StringBuilder sbSQL = new StringBuilder();
 
-            sbSQL.AppendFormat("SELECT USER_ID FROM LOGIN_DETAILS WHERE USER_NAME = '{0}'", objSignup.UserName);
+            sbSQL.AppendFormat("SELECT USER_ID FROM LOGIN_DETAILS WHERE USER_NAME = '{0}'", objSignup.Username);
             command = new SqlCommand(sbSQL.ToString(), cnn);
             dataReader = command.ExecuteReader();
             if (dataReader.Read())
@@ -32,13 +33,13 @@ namespace DocConnectAPI.Controllers
             {
                 dataReader.Close();
                 sbSQL.Clear();
-                sbSQL.AppendFormat("INSERT INTO LOGIN_DETAILS(USER_NAME, PASSWORD) VALUES('{0}', '{1}')", objSignup.UserName, objSignup.UserPassword);
+                sbSQL.AppendFormat("INSERT INTO LOGIN_DETAILS(USER_NAME, PASSWORD) VALUES('{0}', '{1}')", objSignup.Username, objSignup.Password);
                 command = new SqlCommand(sbSQL.ToString(), cnn);
                 command.ExecuteNonQuery();
 
                 dataReader.Close();
                 sbSQL.Clear();
-                sbSQL.AppendFormat("SELECT USER_ID FROM LOGIN_DETAILS WHERE USER_NAME = '{0}'", objSignup.UserName);
+                sbSQL.AppendFormat("SELECT USER_ID FROM LOGIN_DETAILS WHERE USER_NAME = '{0}'", objSignup.Username);
                 command = new SqlCommand(sbSQL.ToString(), cnn);
                 dataReader = command.ExecuteReader();
                 if (dataReader.Read())
@@ -48,14 +49,14 @@ namespace DocConnectAPI.Controllers
 
                 dataReader.Close();
                 sbSQL.Clear();
-                sbSQL.AppendFormat("INSERT INTO USER_TABLE(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, IS_DOCTOR) VALUES({0}, '{1}', '{2}', '{3}', '{4}')", objSignup.UserId, objSignup.FirstName, objSignup.LastName, objSignup.Email, objSignup.IsDoctor);
+                sbSQL.AppendFormat("INSERT INTO USER_TABLE(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, IS_DOCTOR) VALUES({0}, '{1}', '{2}', '{3}', '{4}')", objSignup.UserId, objSignup.FirstName, objSignup.LastName, objSignup.Email, objSignup.IsPractitioner);
                 command = new SqlCommand(sbSQL.ToString(), cnn);
                 command.ExecuteNonQuery();
 
                 dataReader.Close();
                 sbSQL.Clear();
-                if (objSignup.IsDoctor)
-                    sbSQL.AppendFormat("INSERT INTO DOCTOR(USER_ID) VALUES({0})", objSignup.UserId);
+                if (objSignup.IsPractitioner)
+                    sbSQL.AppendFormat("INSERT INTO DOCTOR(USER_ID, DEGREE, FIELD_OF_PRACTICE) VALUES({0}, '{1}', '{2}')", objSignup.UserId, objSignup.Credentials, objSignup.Specialty);
                 else
                     sbSQL.AppendFormat("INSERT INTO PATIENT(USER_ID) VALUES({0})", objSignup.UserId);
                 command = new SqlCommand(sbSQL.ToString(), cnn);
--- DocConnectAPI/Models/SignupModel.cs
+++ DocConnectAPI/Models/SignupModel.cs
@@ -3,13 +3,13 @@
     public class SignupModel
     {
         public int UserId { get; set; }
-        public string DisplayName { get; set; }
-        public string UserName { get; set; }
-        public string UserPassword { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public bool IsAdmin { get; set; }
-        public bool IsDoctor { get; set; }
+        public bool IsPractitioner { get; set; }
+        public string Specialty { get; set; }
+        public string Credentials { get; set; }
     }
 }
